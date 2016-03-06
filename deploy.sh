af90f2145f5e43a2a7cdaf0b8d57f876b463fd91
7sDream <didislover@gmail.com>
2016-03-06 14:50:16 +0800

    change deploy logic

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,6 @@
 # Useage:
 #   Auto deploy site to apache.
 #
-# Arguments:
-#   $1: Website domain
-#   $2: Python env dir, Optional
-
-if [ -z "$1" ]; then
-    echo "No argument!"
-    exit 1
-fi
 
 cd "$(dirname $0)"
 
@@ -18,17 +10,25 @@ SCRIPT=`realpath $0`
 BASE_DIR=`dirname ${SCRIPT}`
 PYTHON_SITE_PACKAGE=`python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 
+if [ ! -f 'db.sqlite3' ]; then
+    # first deploy
+    IS_FIRST_DEPLOY=true
+else
+    IS_FIRST_DEPLOY=false
+fi
+
+# update repo
 # git pull
 
-python3 manage.py makemigrations
-
+# build database
+python3 manage.py makemigrations blog
 python3 manage.py migrate
 
-sed -e "s#%BASE_DIR%#${BASE_DIR}#" -e "s#%domain%#$1#" deploy/apache.conf.template -e "s#%PY_LIB%#$PYTHON_SITE_PACKAGE#" > "$1.conf"
-
-sudo mv -f "$1.conf" "/etc/apache2/sites-available/$1.conf"
-
-sudo a2ensite "$1"
-
-sudo service apache2 reload
-
+# Is first deploy, configure apache
+if [ "${IS_FIRST_DEPLOY}" = true ]; then
+    python3 manage.py createsuperuser
+    sed -e "s#%BASE_DIR%#${BASE_DIR}#" -e "s#%PY_LIB%#$PYTHON_SITE_PACKAGE#" deploy/apache.conf.template > "0v0.link.conf"
+    sudo mv -f "0v0.link.conf" "/etc/apache2/sites-available/0v0.link.conf"
+    sudo a2ensite 0v0.link
+    sudo service apache2 reload
+fi

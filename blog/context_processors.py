663d2b07195d8b55e87e70047e630736eeb02222
7sDream <didislover@gmail.com>
2016-03-07 21:40:23 +0800

    add about to nav

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,12 +1,17 @@
 from django.http import HttpRequest
 
-from .models import Category
+from .models import Category, Post
 from .apps import BlogConfig
 
 
 def blog_conf(request: HttpRequest):
     first_level_categories = Category.objects.filter(parent=None)
+    try:
+        about = Post.objects.get(slug='about')
+    except Post.DoesNotExist:
+        about = None
     return {
         'blog_conf': BlogConfig.blog_settings,
         'categories': first_level_categories,
+        'about': about,
     }
